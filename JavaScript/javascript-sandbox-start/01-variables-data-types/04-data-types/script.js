822d6126248181940addae90d1834f6a865ad5f5
Dustin Turner <dustin.b.turner@outlook.com>
2023-02-28 08:47:39 -0600

    data types

---
+++ JavaScript/javascript-sandbox-start/01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,46 @@
+// String
+
+const firstName = 'Sara';
+
+// Number
+const age = 30;
+const temp = 98.9;
+
+// Boolean
+const hasKids = true;
+
+// Null
+const aptNumber = null;
+// Typeof will tell you its an object
+
+// Undefined
+// let score;
+const score = undefined;
+
+// Symbol
+const id = Symbol('id');
+
+// BigInt
+const n = 9007199254740991n;
+
+// Reference Types
+
+const numbers = [1, 2, 3, 4];
+
+const person = {
+	name: 'Brad',
+};
+
+function sayHello() {
+	console.log('Hello');
+}
+
+//Arrays
+
+// Object Literals
+
+// Functions
+
+const output = sayHello;
+
+console.log(output, typeof output);
