c035a823903fb36d48b581a71e7bc6e02473e062
Dustin Turner <dustin.b.turner@outlook.com>
2023-03-02 09:00:54 -0600

    iife

---
+++ JavaScript/javascript-sandbox-start/03-functions-scope/08-iife/script.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Function Expression
+
+(function () {
+	const user = 'John';
+	console.log(user);
+	const hello = () => console.log('Hello from the IIFE');
+	hello();
+})();
+
+(function (name) {
+	console.log('Hello ' + name);
+})('Shawn');
