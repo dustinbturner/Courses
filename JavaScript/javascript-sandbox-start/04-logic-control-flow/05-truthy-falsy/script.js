667bdf843d583425353eff512cf4eb6c363202c9
Dustin Turner <dustin.b.turner@outlook.com>
2023-03-03 23:34:42 -0600

    saved progress

---
+++ JavaScript/javascript-sandbox-start/04-logic-control-flow/05-truthy-falsy/script.js
@@ -0,0 +1,15 @@
+// Falsy values
+// -  false
+// - 0
+// - "" or ''(Empty String)
+// - null
+// - undefined
+// - NaN
+
+// Truthy Values
+// - Everything that is not falsy
+// - true
+// - '0' (0 in a string)
+// - [] Empty Array
+// - {} Empty Object
+// function () {} (empty function)
